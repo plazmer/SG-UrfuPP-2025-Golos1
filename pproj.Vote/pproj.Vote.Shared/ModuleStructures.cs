727896535b5e6daba5c9655e96e4267de579b0c6
EmuTheGreat <thegreatfrut@mail.ru>
2025-11-18 00:06:18 +0500

    Описание свойств дто моделей [Rx]

--- pproj.Vote/pproj.Vote.Shared/ModuleStructures.cs
+++ pproj.Vote/pproj.Vote.Shared/ModuleStructures.cs
@@ -14,22 +14,22 @@ namespace pproj.Vote.Structures.Module
   partial class PollCreateDto
   {
     /// <summary>
-    /// Тема опроса.
+    /// Тема опроса
     /// </summary>
     public string Subject { get; set; }
 
     /// <summary>
-    /// Описание / вопрос.
+    /// Описание / вопрос
     /// </summary>
     public string Description { get; set; }
 
     /// <summary>
-    /// Разрешён ли выбор нескольких вариантов.
+    /// Разрешён ли выбор нескольких вариантов
     /// </summary>
     public bool IsMultipleChoice { get; set; }
 
     /// <summary>
-    /// Тексты вариантов ответа.
+    /// Тексты вариантов ответа
     /// </summary>
     public List<string> Options { get; set; }
   }
@@ -40,13 +40,40 @@ namespace pproj.Vote.Structures.Module
   [Public]
   partial class PollDto
   {
+    /// <summary>
+    /// Идентификатор опроса
+    /// </summary>
     public long Id { get; set; }
+    
+    /// <summary>
+    /// Заголовок опроса
+    /// </summary>
     public string Subject { get; set; }
+    
+    /// <summary>
+    /// Описаниме опроса
+    /// </summary>
     public string Description { get; set; }
+    
+    /// <summary>
+    /// Один/Несколько вариантов ответа
+    /// </summary>
     public bool IsMultipleChoice { get; set; }
+    
+    /// <summary>
+    /// Статус опроса
+    /// </summary>
     public string StatusVote { get; set; } // Draft / Active / Closed
-    public System.Collections.Generic.List<pproj.Vote.Structures.Module.IPollOptionDto> Options { get; set; }
-    public long? MyVoteOptionId { get; set; }
+    
+    /// <summary>
+    /// Варианты опроса
+    /// </summary>
+    public List<IPollOptionDto> Options { get; set; }
+    
+    /// <summary>
+    /// Выбранные варианты для пользователя
+    /// </summary>
+    public long? MyVoteOptionId { get; set; } //TODO List<long>
   }
 
   /// <summary>
@@ -55,9 +82,19 @@ namespace pproj.Vote.Structures.Module
   [Public]
   partial class PollOptionDto
   {
+    /// <summary>
+    /// Идентификатор варинта ответа
+    /// </summary>
     public long Id { get; set; }
+    
+    /// <summary>
+    /// Текст варианта
+    /// </summary>
     public string Text { get; set; }
+    
+    /// <summary>
+    /// Количество голосов
+    /// </summary>
     public int? VotesCount { get; set; }
   }
-
 }
